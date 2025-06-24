2814e2c81133c52faa201cbc6a6829ca9209542d
andreyshevtsov2006 <andreyshevtsov688@gmail.com>
2025-06-24 19:04:31 +0300

    Add files via upload

---
+++ andrei.sql
@@ -0,0 +1,405 @@
+CREATE DATABASE  IF NOT EXISTS `circus` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `circus`;
+-- MySQL dump 10.13  Distrib 8.0.42, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: circus
+-- ------------------------------------------------------
+-- Server version	8.0.42
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `animals`
+--
+
+DROP TABLE IF EXISTS `animals`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `animals` (
+  `animal_id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) DEFAULT NULL,
+  `species` varchar(50) DEFAULT NULL,
+  `age` int DEFAULT NULL,
+  `trainer_id` int DEFAULT NULL,
+  `health_status` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`animal_id`),
+  KEY `trainer_id` (`trainer_id`),
+  CONSTRAINT `animals_ibfk_1` FOREIGN KEY (`trainer_id`) REFERENCES `artists` (`artist_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `animals`
+--
+
+LOCK TABLES `animals` WRITE;
+/*!40000 ALTER TABLE `animals` DISABLE KEYS */;
+INSERT INTO `animals` VALUES (1,'Барсик','тигр',4,4,'здоров'),(2,'Мурка','лев',5,4,'здоров'),(3,'Рекс','собака',3,4,'травма лапы'),(4,'Кеша','попугай',2,2,'здоров');
+/*!40000 ALTER TABLE `animals` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+/*!50003 CREATE*/ /*!50017 DEFINER=`root`@`localhost`*/ /*!50003 TRIGGER `animal_health` AFTER UPDATE ON `animals` FOR EACH ROW BEGIN
+    IF NEW.age > 10 THEN
+        UPDATE Animals
+        SET health_status = 'требуется осмотр'
+        WHERE animal_id = NEW.animal_id;
+    END IF;
+END */;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+
+--
+-- Table structure for table `artist_performance`
+--
+
+DROP TABLE IF EXISTS `artist_performance`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `artist_performance` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `artist_id` int DEFAULT NULL,
+  `performance_id` int DEFAULT NULL,
+  `role_description` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `artist_id` (`artist_id`),
+  KEY `performance_id` (`performance_id`),
+  CONSTRAINT `artist_performance_ibfk_1` FOREIGN KEY (`artist_id`) REFERENCES `artists` (`artist_id`),
+  CONSTRAINT `artist_performance_ibfk_2` FOREIGN KEY (`performance_id`) REFERENCES `performances` (`performance_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `artist_performance`
+--
+
+LOCK TABLES `artist_performance` WRITE;
+/*!40000 ALTER TABLE `artist_performance` DISABLE KEYS */;
+INSERT INTO `artist_performance` VALUES (1,1,1,'Главный акробат'),(2,2,1,'Жонглёр'),(3,3,2,'Клоун'),(4,4,3,'Дрессировщик животных'),(5,2,3,'Помощник');
+/*!40000 ALTER TABLE `artist_performance` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `artists`
+--
+
+DROP TABLE IF EXISTS `artists`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `artists` (
+  `artist_id` int NOT NULL AUTO_INCREMENT,
+  `full_name` varchar(100) NOT NULL,
+  `genre` varchar(50) DEFAULT NULL,
+  `experience_years` int DEFAULT NULL,
+  `phone` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`artist_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `artists`
+--
+
+LOCK TABLES `artists` WRITE;
+/*!40000 ALTER TABLE `artists` DISABLE KEYS */;
+INSERT INTO `artists` VALUES (1,'Иван Иванов','акробат',8,'+7 912 345-67-89'),(2,'Мария Петрова','жонглёр',5,'+7 903 123-45-67'),(3,'Алексей Смирнов','клоун',10,'+7 925 456-78-90'),(4,'Елена Кузнецова','дрессировщик',7,'+7 910 987-65-43');
+/*!40000 ALTER TABLE `artists` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `employees`
+--
+
+DROP TABLE IF EXISTS `employees`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `employees` (
+  `employee_id` int NOT NULL AUTO_INCREMENT,
+  `full_name` varchar(100) DEFAULT NULL,
+  `position` varchar(50) DEFAULT NULL,
+  `salary` decimal(10,2) DEFAULT NULL,
+  `hire_date` date DEFAULT NULL,
+  PRIMARY KEY (`employee_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `employees`
+--
+
+LOCK TABLES `employees` WRITE;
+/*!40000 ALTER TABLE `employees` DISABLE KEYS */;
+INSERT INTO `employees` VALUES (1,'Сергей Орлов','администратор',40000.00,'2020-01-15'),(2,'Елена Иванова','кассир',30000.00,'2021-03-10'),(3,'Дмитрий Козлов','техник',35000.00,'2019-11-05');
+/*!40000 ALTER TABLE `employees` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Temporary view structure for view `performance_artists`
+--
+
+DROP TABLE IF EXISTS `performance_artists`;
+/*!50001 DROP VIEW IF EXISTS `performance_artists`*/;
+SET @saved_cs_client     = @@character_set_client;
+/*!50503 SET character_set_client = utf8mb4 */;
+/*!50001 CREATE VIEW `performance_artists` AS SELECT 
+ 1 AS `performance_id`,
+ 1 AS `title`,
+ 1 AS `artist_name`,
+ 1 AS `role_description`*/;
+SET character_set_client = @saved_cs_client;
+
+--
+-- Table structure for table `performances`
+--
+
+DROP TABLE IF EXISTS `performances`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `performances` (
+  `performance_id` int NOT NULL AUTO_INCREMENT,
+  `title` varchar(100) NOT NULL,
+  `date` date DEFAULT NULL,
+  `time` time DEFAULT NULL,
+  `location` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`performance_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `performances`
+--
+
+LOCK TABLES `performances` WRITE;
+/*!40000 ALTER TABLE `performances` DISABLE KEYS */;
+INSERT INTO `performances` VALUES (1,'Великолепное шоу акробатов','2025-07-01','18:00:00','Главный манеж'),(2,'Цирковая феерия','2025-07-02','19:00:00','Большой зал'),(3,'Шоу с животными','2025-07-03','17:00:00','Малый манеж');
+/*!40000 ALTER TABLE `performances` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Temporary view structure for view `ticket_summary`
+--
+
+DROP TABLE IF EXISTS `ticket_summary`;
+/*!50001 DROP VIEW IF EXISTS `ticket_summary`*/;
+SET @saved_cs_client     = @@character_set_client;
+/*!50503 SET character_set_client = utf8mb4 */;
+/*!50001 CREATE VIEW `ticket_summary` AS SELECT 
+ 1 AS `performance_id`,
+ 1 AS `title`,
+ 1 AS `tickets_sold`,
+ 1 AS `total_revenue`*/;
+SET character_set_client = @saved_cs_client;
+
+--
+-- Table structure for table `tickets`
+--
+
+DROP TABLE IF EXISTS `tickets`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tickets` (
+  `ticket_id` int NOT NULL AUTO_INCREMENT,
+  `performance_id` int DEFAULT NULL,
+  `visitor_id` int DEFAULT NULL,
+  `seat_number` varchar(10) DEFAULT NULL,
+  `price` decimal(10,2) DEFAULT NULL,
+  `purchase_date` datetime DEFAULT NULL,
+  PRIMARY KEY (`ticket_id`),
+  KEY `performance_id` (`performance_id`),
+  KEY `visitor_id` (`visitor_id`),
+  CONSTRAINT `tickets_ibfk_1` FOREIGN KEY (`performance_id`) REFERENCES `performances` (`performance_id`),
+  CONSTRAINT `tickets_ibfk_2` FOREIGN KEY (`visitor_id`) REFERENCES `visitors` (`visitor_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tickets`
+--
+
+LOCK TABLES `tickets` WRITE;
+/*!40000 ALTER TABLE `tickets` DISABLE KEYS */;
+INSERT INTO `tickets` VALUES (1,1,1,'A1',1500.00,'2025-06-20 10:00:00'),(2,1,2,'A2',1500.00,'2025-06-20 11:00:00'),(3,2,3,'B1',1800.00,'2025-06-21 09:30:00'),(4,3,1,'C1',1300.00,'2025-06-22 15:45:00');
+/*!40000 ALTER TABLE `tickets` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+/*!50003 CREATE*/ /*!50017 DEFINER=`root`@`localhost`*/ /*!50003 TRIGGER `check_ticket_price` BEFORE INSERT ON `tickets` FOR EACH ROW BEGIN
+    IF NEW.price < 0 THEN
+        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Цена билета не может быть отрицательной';
+    END IF;
+END */;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+
+--
+-- Table structure for table `visitors`
+--
+
+DROP TABLE IF EXISTS `visitors`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `visitors` (
+  `visitor_id` int NOT NULL AUTO_INCREMENT,
+  `full_name` varchar(100) DEFAULT NULL,
+  `phone` varchar(20) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`visitor_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `visitors`
+--
+
+LOCK TABLES `visitors` WRITE;
+/*!40000 ALTER TABLE `visitors` DISABLE KEYS */;
+INSERT INTO `visitors` VALUES (1,'Андрей Васильев','+7 900 111-22-33','andrey@mail.com'),(2,'Ольга Сидорова','+7 901 222-33-44','olga@mail.ru'),(3,'Наталья Крылова','+7 902 333-44-55','natalia@example.com');
+/*!40000 ALTER TABLE `visitors` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping events for database 'circus'
+--
+
+--
+-- Dumping routines for database 'circus'
+--
+/*!50003 DROP PROCEDURE IF EXISTS `AddTicket` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `AddTicket`(
+    IN p_performance_id INT,
+    IN p_visitor_id INT,
+    IN p_seat_number VARCHAR(10),
+    IN p_price DECIMAL(10,2)
+)
+BEGIN
+    DECLARE seat_taken INT;
+
+    SELECT COUNT(*) INTO seat_taken
+    FROM Tickets
+    WHERE performance_id = p_performance_id AND seat_number = p_seat_number;
+
+    IF seat_taken > 0 THEN
+        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Место уже занято';
+    ELSE
+        INSERT INTO Tickets (performance_id, visitor_id, seat_number, price, purchase_date)
+        VALUES (p_performance_id, p_visitor_id, p_seat_number, p_price, NOW());
+    END IF;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `available_seats` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `available_seats`()
+BEGIN
+    SELECT
+        p.performance_id,
+        p.title,
+        p.date,
+        p.time,
+        p.location,
+        (100 - COUNT(t.ticket_id)) AS free_seats
+    FROM Performances p
+    LEFT JOIN Tickets t ON p.performance_id = t.performance_id
+    GROUP BY p.performance_id, p.title, p.date, p.time, p.location;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+
+--
+-- Final view structure for view `performance_artists`
+--
+
+/*!50001 DROP VIEW IF EXISTS `performance_artists`*/;
+/*!50001 SET @saved_cs_client          = @@character_set_client */;
+/*!50001 SET @saved_cs_results         = @@character_set_results */;
+/*!50001 SET @saved_col_connection     = @@collation_connection */;
+/*!50001 SET character_set_client      = utf8mb4 */;
+/*!50001 SET character_set_results     = utf8mb4 */;
+/*!50001 SET collation_connection      = utf8mb4_0900_ai_ci */;
+/*!50001 CREATE ALGORITHM=UNDEFINED */
+/*!50013 DEFINER=`root`@`localhost` SQL SECURITY DEFINER */
+/*!50001 VIEW `performance_artists` AS select `p`.`performance_id` AS `performance_id`,`p`.`title` AS `title`,`a`.`full_name` AS `artist_name`,`ap`.`role_description` AS `role_description` from ((`performances` `p` join `artist_performance` `ap` on((`p`.`performance_id` = `ap`.`performance_id`))) join `artists` `a` on((`ap`.`artist_id` = `a`.`artist_id`))) */;
+/*!50001 SET character_set_client      = @saved_cs_client */;
+/*!50001 SET character_set_results     = @saved_cs_results */;
+/*!50001 SET collation_connection      = @saved_col_connection */;
+
+--
+-- Final view structure for view `ticket_summary`
+--
+
+/*!50001 DROP VIEW IF EXISTS `ticket_summary`*/;
+/*!50001 SET @saved_cs_client          = @@character_set_client */;
+/*!50001 SET @saved_cs_results         = @@character_set_results */;
+/*!50001 SET @saved_col_connection     = @@collation_connection */;
+/*!50001 SET character_set_client      = utf8mb4 */;
+/*!50001 SET character_set_results     = utf8mb4 */;
+/*!50001 SET collation_connection      = utf8mb4_0900_ai_ci */;
+/*!50001 CREATE ALGORITHM=UNDEFINED */
+/*!50013 DEFINER=`root`@`localhost` SQL SECURITY DEFINER */
+/*!50001 VIEW `ticket_summary` AS select `p`.`performance_id` AS `performance_id`,`p`.`title` AS `title`,count(`t`.`ticket_id`) AS `tickets_sold`,sum(`t`.`price`) AS `total_revenue` from (`performances` `p` left join `tickets` `t` on((`p`.`performance_id` = `t`.`performance_id`))) group by `p`.`performance_id`,`p`.`title` */;
+/*!50001 SET character_set_client      = @saved_cs_client */;
+/*!50001 SET character_set_results     = @saved_cs_results */;
+/*!50001 SET collation_connection      = @saved_col_connection */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-06-24 18:34:47
