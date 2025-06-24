c1d2beca005d9b01d6e4c45e97375cb406f5fa20
andreyshevtsov2006 <andreyshevtsov688@gmail.com>
2025-06-24 19:31:51 +0300

    Add files via upload

--- andrei.sql
+++ andrei.sql
@@ -293,6 +293,31 @@ UNLOCK TABLES;
 --
 -- Dumping routines for database 'circus'
 --
+/*!50003 DROP PROCEDURE IF EXISTS `AddArtist` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `AddArtist`(
+    IN fullName VARCHAR(100),
+    IN genre VARCHAR(50),
+    IN experience INT,
+    IN phone VARCHAR(20)
+)
+BEGIN
+    INSERT INTO Artists (full_name, genre, experience_years, phone)
+    VALUES (fullName, genre, experience, phone);
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
 /*!50003 DROP PROCEDURE IF EXISTS `AddTicket` */;
 /*!50003 SET @saved_cs_client      = @@character_set_client */ ;
 /*!50003 SET @saved_cs_results     = @@character_set_results */ ;
@@ -322,34 +347,6 @@ BEGIN
         INSERT INTO Tickets (performance_id, visitor_id, seat_number, price, purchase_date)
         VALUES (p_performance_id, p_visitor_id, p_seat_number, p_price, NOW());
     END IF;
-END ;;
-DELIMITER ;
-/*!50003 SET sql_mode              = @saved_sql_mode */ ;
-/*!50003 SET character_set_client  = @saved_cs_client */ ;
-/*!50003 SET character_set_results = @saved_cs_results */ ;
-/*!50003 SET collation_connection  = @saved_col_connection */ ;
-/*!50003 DROP PROCEDURE IF EXISTS `available_seats` */;
-/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
-/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
-/*!50003 SET @saved_col_connection = @@collation_connection */ ;
-/*!50003 SET character_set_client  = utf8mb4 */ ;
-/*!50003 SET character_set_results = utf8mb4 */ ;
-/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
-/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
-/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
-DELIMITER ;;
-CREATE DEFINER=`root`@`localhost` PROCEDURE `available_seats`()
-BEGIN
-    SELECT
-        p.performance_id,
-        p.title,
-        p.date,
-        p.time,
-        p.location,
-        (100 - COUNT(t.ticket_id)) AS free_seats
-    FROM Performances p
-    LEFT JOIN Tickets t ON p.performance_id = t.performance_id
-    GROUP BY p.performance_id, p.title, p.date, p.time, p.location;
 END ;;
 DELIMITER ;
 /*!50003 SET sql_mode              = @saved_sql_mode */ ;
@@ -402,4 +399,4 @@ DELIMITER ;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-06-24 18:34:47
+-- Dump completed on 2025-06-24 19:31:30
